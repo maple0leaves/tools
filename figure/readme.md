b47bdbff95c81b9f9db140fb23e43c562dd2c489
maple0leaves <2689339890@qq.com>
2023-10-12 16:47:47 +0800

    update img , readme,comment

---
+++ figure/readme.md
@@ -0,0 +1,8 @@
+# Draw figures 
+
+### 1. use matplotlib to draw a comparison figures
+
+comparison figures in my paper ,as a record and I can use code next time .<br>
+
+![图片怎么不见了呢？快找找](,"draw.py")
+
--- smalldataset/small_dataset.py
+++ smalldataset/small_dataset.py
@@ -38,8 +38,15 @@ def makesmall(imgpath,labelpath):
 if __name__ =="__main__":
 
     files =['test', 'train', 'trainval', 'val']
+    # deletion_ratio : how many do you want to delete ,save 1- deletion_ratio
+    deletion_ratio = 0.5
+    # imgpath is images path
+    # labelpath is labels path
+    # depend on deletion_ratio remove label in labelpath and use label
+    # file name save img in imgpath .
     for file in files:
         imgpath = r'F:\ZZLcode\hand_glove_last\images\\'+file
         labelpath = r'F:\ZZLcode\hand_glove_last\labels\\'+file
+        # labelpath could not change
         delete_labels_by_ratio(labelpath, 0.5)
         makesmall(imgpath=imgpath, labelpath=labelpath)
